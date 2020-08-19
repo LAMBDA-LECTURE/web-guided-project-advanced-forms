6b65f86b49c636e38c33350a6a2709761af7917a
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-19 19:42:40 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -105,8 +105,8 @@ export default function App() {
     const newFriend = {
       username: formValues.username.trim(),
       email: formValues.email.trim(),
-      role: formValues.role.trim(),
-      civil: formValues.civil.trim(),
+      role: formValues.role,
+      civil: formValues.civil,
       // 🔥 STEP 8- WHAT ABOUT HOBBIES?
       hobbies: Object.keys(formValues.hobbies).filter(h => h),
     }
