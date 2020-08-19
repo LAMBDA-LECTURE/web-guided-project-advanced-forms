cd8a2efb47c5fd639adbdcd470ba342006c65cf1
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-19 19:03:15 +0200

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -4,8 +4,8 @@ import * as yup from 'yup'
 const formSchema = yup.object().shape({
   email: yup
     .string()
-    .email("Must be a valid email address.")
-    .required("Must include email address."),
+    .email("Must be a valid email address")
+    .required("Must include email address"),
   password: yup
     .string()
     .min(6, "Passwords must be at least 6 characters long.")
