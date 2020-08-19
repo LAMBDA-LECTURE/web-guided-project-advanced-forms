6111cd8d999f031c8dc8367ce71f049b1452fd74
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-19 19:04:04 +0200

    wip

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -6,10 +6,10 @@ const formSchema = yup.object().shape({
     .string()
     .email("Must be a valid email address")
     .required("Must include email address"),
-  password: yup
+  username: yup
     .string()
-    .min(6, "Passwords must be at least 6 characters long.")
-    .required("Password is Required"),
+    .min(3, "Username must be at least 3 characters long")
+    .required("Username is Required"),
   terms: yup
     .boolean()
     .oneOf([true], "You must accept Terms and Conditions")
