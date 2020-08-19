5e6736b1e5d9d0e540a333d84bb52526d868cfa5
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-19 19:26:47 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -61,18 +61,16 @@ export default function App() {
       })
   }
 
-  const postNewFriend = ({ username, role, email, civil, hobbies }) => {
+  const postNewFriend = newFriend => {
     // 🔥 STEP 6- IMPLEMENT! ON SUCCESS ADD NEWLY CREATED FRIEND TO STATE
     //    helper to [POST] `newFriend` to `http://localhost:4000/friends`
     //    and regardless of success or failure, the form should reset
     axios.post('http://localhost:4000/friends', newFriend)
       .then(res => {
         debugger
-        console.log(res)
       })
       .catch(err => {
         debugger
-        console.log(err)
       })
   }
 
